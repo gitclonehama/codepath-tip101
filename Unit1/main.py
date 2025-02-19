677550124576979ece697622d4a78ca98f4117f2
Hama Pashazadeh <pashazad@msu.edu>
2025-02-19 14:56:14 -0500

    Add Hello World implementation in Unit1/main.py

---
+++ Unit1/main.py
@@ -0,0 +1,24 @@
+###
+### Unit 1
+###
+
+
+"""
+Problem 1: Hello World!
+
+Understand:
+    - What is the correct order to define and call a function within Python?
+    - How do we get a message to print inside a function?
+
+Plan:
+    - Define a function hello_world() that 
+    - Use print() with the string "Hello world!" inside it
+    - Call the function
+"""
+
+# Implement:
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
