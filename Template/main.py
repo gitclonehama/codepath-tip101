9f52cff77e313e97a79a66761ef47cbc659fc3d6
Hama Pashazadeh <pashazad@msu.edu>
2025-02-19 14:55:35 -0500

    Add template for Unit X problem-solving approach in Template/main.py

---
+++ Template/main.py
@@ -0,0 +1,19 @@
+###
+### Unit X
+###
+
+
+""" 
+Problem X:
+
+
+Understand:
+    - Share 2 questions you would ask to help understand the question
+
+Plan:
+    - Write out in plain English what you want to do
+    - Translate each sub-problem into pseudocode
+
+Implement:
+    - Translate the pseudocode into Python and share your final answer
+"""
