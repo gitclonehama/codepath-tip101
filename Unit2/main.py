028a81031b02f51632ec051d5b0a574bd942737a
Hama Pashazadeh <pashazad@msu.edu>
2025-02-24 09:36:08 -0500

    Implement is_subsequence function with detailed plan and example usage in Unit2/main.py

--- Unit2/main.py
+++ Unit2/main.py
@@ -14,11 +14,30 @@ Understand:
     - True if sequence exists, False otherwise
 
 Plan:
-    - 
+    - have a pointer for sequence and lst
+    - loop through lst, incremting both pointers if they match
+    - after the loop is done, if we exhausted sequence, then return True
+    - if we are not at the end of sequence, return false
 
 """
 # Implement
 
+def is_subsequence(lst, sequence):
+    seq_idx = 0
+
+    for i in range(len(lst)):
+
+        if seq_idx >= len(sequence):
+            return True
+
+        if lst[i] == sequence[seq_idx]:
+            seq_idx += 1
+    
+    return False
+
+lst = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+print(is_subsequence(lst, sequence))    # Expected: True
 
 """ 
 Problem 2:
