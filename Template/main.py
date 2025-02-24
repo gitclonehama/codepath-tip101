d818edb15152b87f6fdb584e473d518e9ba17ba1
Hama Pashazadeh <pashazad@msu.edu>
2025-02-23 20:34:32 -0500

    Add structured problem templates for Unit 2 in main.py

--- Template/main.py
+++ Template/main.py
@@ -17,3 +17,143 @@ Plan:
 Implement:
     - Translate the pseudocode into Python and share your final answer
 """
+
+
+""" 
+Problem 1:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 2:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 3:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 4:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 5:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 6:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 7:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 8:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 9:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 10:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
--- Unit2/main.py
+++ Unit2/main.py
@@ -1,19 +1,143 @@
 ###
-### Unit X
+### Unit 2
 ###
 
 
 """ 
-Problem X:
+Problem 1:
 
 
 Understand:
-    - Share 2 questions you would ask to help understand the question
+
 
 Plan:
-    - Write out in plain English what you want to do
-    - Translate each sub-problem into pseudocode
 
-Implement:
-    - Translate the pseudocode into Python and share your final answer
+
 """
+# Implement
+
+
+""" 
+Problem 2:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 3:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 4:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 5:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 6:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 7:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 8:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 9:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 10:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
