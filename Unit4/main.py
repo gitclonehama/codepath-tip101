d0a66e9ae1431b1ee2e24f712a5e6cacc26b5d45
Hama Pashazadeh <pashazad@msu.edu>
2025-03-15 12:35:38 -0400

    Push Unit4 problems

--- Unit4/main.py
+++ Unit4/main.py
@@ -4,22 +4,6 @@
 
 
 """ 
-Problem X:
-
-
-Understand:
-    - Share 2 questions you would ask to help understand the question
-
-Plan:
-    - Write out in plain English what you want to do
-    - Translate each sub-problem into pseudocode
-
-Implement:
-    - Translate the pseudocode into Python and share your final answer
-"""
-
-
-""" 
 Problem 1: Prime Number
 
 Write a function is_prime() that takes in a positive integer n and returns True if it is a prime 
