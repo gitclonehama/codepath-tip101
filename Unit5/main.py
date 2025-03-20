5c92afdb1aaa2348b72dc275b8edf1e95faa9747
Hama Pashazadeh <pashazad@msu.edu>
2025-03-19 20:57:22 -0400

    Add choose method to Pokemon class with conditional logic

--- Unit5/main.py
+++ Unit5/main.py
@@ -121,3 +121,46 @@ my_pokemon.catch()
 my_pokemon.print_pokemon()
 
 
+""" 
+Problem 5: Choose Pokemon
+
+Understand:
+- How do I add a conditional method?
+- How should the method output be formatted?
+
+Plan:
+- Add a choose method to the Pokemon class
+- Implement the conditional logic
+- Demonstrate with examples
+"""
+# Implement
+class Pokemon:
+    def __init__(self, name, types):
+        self.name = name
+        self.types = types
+        self.is_caught = False
+    
+    def print_pokemon(self):
+        print({
+            "name": self.name,
+            "types": self.types,
+            "is_caught": self.is_caught
+        })
+    
+    def catch(self):
+        self.is_caught = True
+    
+    def choose(self):
+        if self.is_caught:
+            print(f"{self.name} I choose you!")
+        else:
+            print(f"{self.name} is wild! Catch them if you can!")
+
+my_pokemon = Pokemon("rattata", ["Normal"])
+my_pokemon.print_pokemon()
+
+my_pokemon.choose()
+my_pokemon.catch()
+my_pokemon.choose()
+
+
