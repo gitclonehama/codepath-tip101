0dd8402d731dc7c85f460c96b92036c0aa71b561
Hama Pashazadeh <pashazad@msu.edu>
2025-03-19 20:57:40 -0400

    Add attack method to Pokemon class for battling functionality

--- Unit5/main.py
+++ Unit5/main.py
@@ -164,3 +164,36 @@ my_pokemon.catch()
 my_pokemon.choose()
 
 
+""" 
+Problem 6: Battle Pokemon
+
+Understand:
+- How do I implement a method that affects another object?
+- What conditions need to be checked?
+
+Plan:
+- Create the Pokemon class with hp and damage
+- Implement the attack method with the specified logic
+"""
+# Implement
+class Pokemon():
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp # hit points
+        self.damage = damage # The amount of damage this pokemon does to its opponent every attack
+        
+    def attack(self, opponent):
+        opponent.hp -= self.damage
+        if opponent.hp <= 0:
+            opponent.hp = 0
+            print(f"{opponent.name} fainted")
+        else:
+            print(f"{self.name} dealt {self.damage} damage to {opponent.name}")
+
+pikachu = Pokemon("Pikachu", 35, 20)
+bulbasaur = Pokemon("Bulbasaur", 45, 30)
+
+opponent = bulbasaur
+pikachu.attack(opponent)
+
+
