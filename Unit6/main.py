ee9be5419f39c2d66982d0ea968f070629e5af0e
Hama Pashazadeh <pashazad@msu.edu>
2025-03-23 14:41:09 -0400

    Implement problem 2

--- Unit6/main.py
+++ Unit6/main.py
@@ -76,3 +76,43 @@ def count_element(head, val):
 # Space Complexity: O(1) as we only use a constant amount of extra space regardless of input size.
 
 
+""" 
+Problem 3: Remove Tail
+
+Understand:
+    - We need to fix a bug in the remove_tail function
+    - The function should remove the last node of a linked list
+    - Current bug: The function finds the last node instead of the second-to-last node
+
+Plan:
+    - Fix the while loop condition to stop at the second-to-last node
+"""
+# Implement
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+        
+# Helper function to print the linked list
+def print_list(node):
+    current = node
+    while current:
+        print(current.value, end=" -> " if current.next else "")
+        current = current.next
+    print()
+
+def remove_tail(head):
+    if head is None: # If the list is empty, return None
+        return None
+    if head.next is None: # If there's only one node, removing it leaves the list empty
+        return None 
+    
+    # Start from the head and find the second-to-last node
+    current = head
+    while current.next.next: # Fixed: Stop at second-to-last node
+        current = current.next
+
+    current.next = None # Remove the last node by setting second-to-last node's next to None
+    return head
+
+
