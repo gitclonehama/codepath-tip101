0e5196ea310d62be8dc14e55d5d93e6a9cfaf1f5
Hama Pashazadeh <pashazad@msu.edu>
2025-04-08 20:50:41 -0400

    Copy template over to unit 8

---
+++ Unit8/main.py
@@ -0,0 +1,159 @@
+###
+### Unit 8
+###
+
+
+""" 
+Problem X:
+
+
+Understand:
+    - Share 2 questions you would ask to help understand the question
+
+Plan:
+    - Write out in plain English what you want to do
+    - Translate each sub-problem into pseudocode
+
+Implement:
+    - Translate the pseudocode into Python and share your final answer
+"""
+
+
+""" 
+Problem 1:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 2:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 3:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 4:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 5:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 6:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 7:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 8:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 9:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
+
+
+""" 
+Problem 10:
+
+
+Understand:
+
+
+Plan:
+
+
+"""
+# Implement
